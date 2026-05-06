faf4ce09cdaa476783421034e49ea04c872e513e
hamzaamir-design <hamzaamir9733@gmail.com>
2026-05-06 08:13:00 +0500

    PolyAlphabetic Cipher Done.

---
+++ Substitution/Polyalphabetic/VigenereCipher.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+string generateKey(string text, string key) {
+    for (int i = 0; key.length() < text.length(); i++)
+        key += key[i];
+    return key;
+}
+
+string encrypt(string text, string key) {
+    string result = "";
+    for (int i = 0; i < text.length(); i++) {
+        result += char((text[i] + key[i]) % 26 + 'A');
+    }
+    return result;
+}
+
+string decrypt(string cipher, string key) {
+    string result = "";
+    for (int i = 0; i < cipher.length(); i++) {
+        result += char((cipher[i] - key[i] + 26) % 26 + 'A');
+    }
+    return result;
+}
+
+int main() {
+    string text = "HELLO";
+    string key = "KEY";
+
+    key = generateKey(text, key);
+
+    string enc = encrypt(text, key);
+    cout << "Encrypted: " << enc << endl;
+
+    string dec = decrypt(enc, key);
+    cout << "Decrypted: " << dec << endl;
+
+    return 0;
+}
