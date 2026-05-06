58a63956eeb5d66609151de5b7861cef5ea87b3a
hamzaamir-design <hamzaamir9733@gmail.com>
2026-05-06 08:09:28 +0500

    One time pad Cipher Done.

---
+++ Substitution/OneTimePadCipher.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+string encryptDecrypt(string text, string key) {
+    string result = "";
+
+    for (int i = 0; i < text.length(); i++) {
+        result += char(text[i] ^ key[i]); // XOR
+    }
+
+    return result;
+}
+
+int main() {
+    string text = "HELLO";
+    string key  = "XMCKL"; // must be same length
+
+    string enc = encryptDecrypt(text, key);
+    cout << "Encrypted: " << enc << endl;
+
+    string dec = encryptDecrypt(enc, key);
+    cout << "Decrypted: " << dec << endl;
+
+    return 0;
+}
